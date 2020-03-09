8858b5bcf50d62781dcf299478a3f1c9752b2b5b
Igor Garcia <alexandre.igor64@gmail.com>
2020-03-09 14:11:50 -0300

    inclusao gets

--- Aluno.java
+++ Aluno.java
@@ -1,6 +1,14 @@
-public class Aluno{
-   private String nome;
-   private int id;
-   
+
+public class Aluno {
+	private String nome;
+	private int id;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
 }
-  
